985b8fc01bcdb36be5ec62f8c513d5ce3b686d93
UsamaBinImran-Student <i247800@isb.nu.edu.pk>
2026-09-11 11:52:05 +0500

    Change server port from 5000 to 8000

--- nids_api_server.py
+++ nids_api_server.py
@@ -734,4 +734,4 @@ if __name__ == '__main__':
     print(f"    GET  /api/health         — Health check")
     print(f"\n  Running on http://0.0.0.0:5000")
     print("="*55 + "\n")
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host="0.0.0.0", port=8000, debug=False)
